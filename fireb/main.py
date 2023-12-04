4c70bc3311b8cdce10224bb9b51b29d04cb0eaa7
dshaw0004 <114005178+dshaw0004@users.noreply.github.com>
2023-12-04 22:25:37 +0530

    firebase added 4

--- fireb/main.py
+++ fireb/main.py
@@ -1,7 +1,7 @@
 import os
 
 import firebase_admin
-from firebase_admin import cert, credentials
+from firebase_admin import credentials
 
 cred = credentials.RefreshToken("/etc/secrets/fire.json")
 app = firebase_admin.initialize_app(cred)

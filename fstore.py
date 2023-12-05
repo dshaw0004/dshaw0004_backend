8fd6a2f404a15ca638eab2332c26aa7d5ffb4fa0
dshaw0004 <114005178+dshaw0004@users.noreply.github.com>
2023-12-05 11:16:03 +0530

    firebase added 8

--- fstore.py
+++ fstore.py
@@ -1,23 +1,24 @@
 from firebase_admin import firestore
 
-from .fire import app
+from fire import app
 
 db = firestore.client(app)
 
 
 def get_all_message():
-  ref = db.collection("messages")
-  docs = ref.stream()
-  all_messages = []
-  for doc in docs:
-    print(f"{doc.id} => {doc.to_dict()}")
-    all_messages.append(doc.to_dict())
-  print(all_messages)
-  return all_messages
-  
+    ref = db.collection("messages")
+    docs = ref.stream()
+    all_messages = []
+    for doc in docs:
+        print(f"{doc.id} => {doc.to_dict()}")
+        all_messages.append(doc.to_dict())
+    print(all_messages)
+    return all_messages
 
-def add_new_message(message:str, senderName:str, senderContact:str):
-  
-  no_of_message = 1 
-  doc_ref = db.collection("messages").document(f"demo{no_of_message}")
-  doc_ref.set({"message":message, "senderName":senderName, "senderContact": senderContact})
+
+def add_new_message(message: str, senderName: str, senderContact: str):
+
+    no_of_message = 1
+    doc_ref = db.collection("messages").document(f"demo{no_of_message}")
+    doc_ref.set({"message": message, "senderName": senderName,
+                "senderContact": senderContact})

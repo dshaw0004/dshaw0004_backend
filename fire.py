0f3854fa1f0fccbcb69c30d3e66f3e8ad53c0e90
dshaw0004 <114005178+dshaw0004@users.noreply.github.com>
2023-12-05 12:01:17 +0530

    firebase added 9

--- fire.py
+++ fire.py
@@ -3,5 +3,5 @@ import os
 import firebase_admin
 from firebase_admin import credentials
 
-cred = credentials.RefreshToken("fire.json")
+cred = credentials.Certificate("fire.json")
 app = firebase_admin.initialize_app(cred)

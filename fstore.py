05dbd6267f3b36e48171269209daa7ce429c0f90
dshaw0004 <114005178+dshaw0004@users.noreply.github.com>
2023-12-05 15:00:01 +0530

    login added

--- fstore.py
+++ fstore.py
@@ -1,3 +1,5 @@
+from random import randint
+
 from firebase_admin import firestore
 
 from fire import app
@@ -10,15 +12,15 @@ def get_all_message():
     docs = ref.stream()
     all_messages = []
     for doc in docs:
-        print(f"{doc.id} => {doc.to_dict()}")
+        #        print(f"{doc.id} => {doc.to_dict()}")
         all_messages.append(doc.to_dict())
-    print(all_messages)
+#    print(all_messages)
     return all_messages
 
 
 def add_new_message(message: str, senderName: str, senderContact: str):
 
-    no_of_message = 1
-    doc_ref = db.collection("messages").document(f"demo{no_of_message}")
+    no_of_message = randint * randint
+    doc_ref = db.collection("messages").document(f"devp{no_of_message}")
     doc_ref.set({"message": message, "senderName": senderName,
                 "senderContact": senderContact})
--- main.py
+++ main.py
@@ -1,22 +1,86 @@
-from flask import Flask, render_template
+import os
 
-from fstore import get_all_message
+from flask import Flask, render_template, request
+from flask_cors import CORS, cross_origin
+
+from fstore import add_new_message, get_all_message
 
 app = Flask(__name__)
+CORS(app, resources={
+    r'/getallmsg': {
+        "origins": [
+            'https://dshaw0004.netlify.app',
+            'https://dshaw0004.web.app',
+            'https://dshaw0004.firebaseapp.com',
+            'https://dshaw0004.vercel.app',
+            # 'http://192.168.199.249:6970/index.html'
+        ]
+    },
+    r'/addnewmsg': {
+        "origins": [
+            'https://dshaw0004.netlify.app',
+            'https://dshaw0004.web.app',
+            'https://dshaw0004.firebaseapp.com',
+            'https://dshaw0004.vercel.app',
+            # 'http://192.168.199.249:6970/index.html'
+        ]
+    },
+    r'/': {
+        "origins": [
+            "*"
+        ]
+    },
+    r'/login': {
+        "origins": [
+            "*"
+        ]
+    }
+})
 
 
 @app.route("/")
 def index():
     return {
-        'name': 'dshaw0004',
-        'lang': 'python'
+        'username': 'dshaw0004',
+        'portfolio': 'https://dshaw0004.netlify.app',
+        'fullname': 'Dipankar Shaw',
+        'linkedin': 'https://www.linkedin.com/in/dshaw0004',
+        'twitter': 'https://twitter.com/dshaw0004',
+        'github': 'https://github.com/dshaw0004'
     }
 
 
-@app.route("/allmes")
+@app.route("/getallmsg")
 def allmes():
     allmess = get_all_message()
     return allmess
+
+
+@app.route("/addnewmsg", methods=['post'])
+def addnewmsg():
+    data = request.json
+    add_new_message(message=data.get('message'), senderName=data.get(
+        'senderName'), senderContact=data.get('senderContact'))
+    return {
+        'your_name': data.get('senderName'),
+        'your_message': data.get('message'),
+        'your_contact': data.get('senderContact')
+    }
+
+
+@app.route("/login", methods=['post'])
+def login():
+    data = request.json
+    if data.get('email') == os.getenv("email") and data.get("password") == os.getenv("password"):
+        return {
+            'access': 'granted',
+            'id': 'admin'
+        }
+    else:
+        return {
+            'access': 'denied',
+            'id': 'unknown'
+        }
 
 
 if __name__ == "__main__":

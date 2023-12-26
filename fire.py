64e91d6c72a18547ced822331de3b986a0ad0daf
dshaw0004 <114005178+dshaw0004@users.noreply.github.com>
2023-12-26 19:16:43 +0530

    appstore added fixed

--- fire.py
+++ fire.py
@@ -6,5 +6,5 @@ from firebase_admin import credentials
 cred = credentials.Certificate("fire.json")
 app1 = firebase_admin.initialize_app(cred)
 
-app1 = firebase_admin.initialize_app(
-    credentials.Certificate("pyapps_config.json"))
+cred2 = credentials.Certificate("pyapps_config.json")
+app2 = firebase_admin.initialize_app(cred2)

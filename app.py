2cbfbdffb757b70940270d9687fcda01a5f088c2
dshaw0004 <dipankarshaw692@gmail.com>
2024-11-21 12:57:02 +0530

    flask socket.io for localchat app is added to it.

---
+++ app.py
@@ -0,0 +1,32 @@
+from uuid import uuid4
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+from flask_socketio import join_room, leave_room
+from flask_socketio import send, emit
+from flask_cors import CORS
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+socketio = SocketIO(app, cors_allowed_origins="http://localhost:5173")
+
+@socketio.on('join')
+def on_join(data):
+    username = data['username']
+    room = data['room']
+    print(room)
+    join_room(room)
+    # send(username + ' has entered the room.', to=room)
+    emit('newjoin', {'username': username}, broadcast=True, include_self=False, to=room)
+
+@socketio.on('text')
+def text(msg, room):
+    # print(data)
+    emit('meessageBroadcast', {'message': msg, 'id': str(uuid4())}, broadcast=True, include_self=False, to=room)
+
+@socketio.on('client_disconnecting')
+def disconnect_details(username, room):
+    emit('remove', {'username': f'{username} user disconnected.'}, broadcast=True, include_self=False, to=room)
+
+if __name__ == '__main__':
+    socketio.run(app, port=3428, debug=True)
--- fire.py
+++ fire.py
@@ -3,11 +3,5 @@ import os
 import firebase_admin
 from firebase_admin import credentials
 
-cred = credentials.Certificate("fire.json")
-app1 = firebase_admin.initialize_app(cred, name="personal")
-
-cred2 = credentials.Certificate("pyapps_config.json")
-app2 = firebase_admin.initialize_app(cred2, name="appstore")
-
-cred3 = credentials.Certificate("appbank_config.json")
-app3 = firebase_admin.initialize_app(cred2, name="appbank")
+cred = credentials.Certificate("firekey.json")
+app = firebase_admin.initialize_app(cred, name="appbank")

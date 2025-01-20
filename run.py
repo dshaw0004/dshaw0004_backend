3885f5a2b32617b9e5aa0202c6408e61ac34f49f
dshaw0004 <dipankarshaw692@gmail.com>
2025-01-20 12:31:49 +0530

    backend for chat app 2

--- run.py
+++ run.py
@@ -4,5 +4,5 @@ from app import db
 if '__main__' == __name__:
     with app.app_context():
         db.create_all()
-        socketio.run(app, port=8080)
+        socketio.run(app, port=5000, host='0.0.0.0')
     # app.run(host='0.0.0.0', debug=True)

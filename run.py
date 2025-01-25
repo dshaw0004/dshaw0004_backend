541451795fd82d1ab2f73fc31adec179f717c6d1
dshaw0004 <dipankarshaw692@gmail.com>
2025-01-25 21:18:40 +0530

    hope that the runtimeerror will be fixed

--- run.py
+++ run.py
@@ -6,5 +6,5 @@ if '__main__' == __name__:
     print('going to run the app.')
     with app.app_context():
         db.create_all()
-        socketio.run(app, port=5000, host='0.0.0.0')
+        socketio.run(app, port=5000, host='0.0.0.0', allow_unsafe_werkzeug=True )
     # app.run(host='0.0.0.0', debug=True)

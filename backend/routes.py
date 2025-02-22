117333b01df5dadc8a3340ebee36665bec6a1271
dshaw0004 <dipankarshaw692@gmail.com>
2025-02-22 22:42:39 +0530

    feat: endpoint for digital gold

--- backend/routes.py
+++ backend/routes.py
@@ -1,4 +1,7 @@
+import os
+import datetime
 import sqlite3
+import requests
 from backend import app, get_db, socketio
 from flask import request, jsonify
 from flask_socketio import join_room
@@ -87,6 +90,37 @@ def delete_app_by_name():
     cur.execute('DELETE FROM AppInfo WHERE name = ?', (name,))
 
     return jsonify({'message': 'App deleted successfully'}), 200
+
+@app.route('/digital-gold')
+def digital_gold():
+    current_date = datetime.date.today()
+
+    today = f'{current_date.year}-{current_date.month}-{current_date.day}'
+    tomorrow = f'{current_date.year}-{current_date.month}-{current_date.day + 1}'
+
+    # filename: str = 'gold-price-today.json'
+    # if os.path.exists(filename):
+    #     with open(filename, 'r') as file:
+    #         file_content = file.read()
+    #         if file_content:
+    #             quote_dict = json.loads(file_content)
+    #             if quote_dict.get('date') == today:
+    #                 return quote_dict
+    #     "https://www.safegold.com/user-trends/gold-rates?start_date=2017-06-23&end_date=2024-06-23&frequency=d"
+    res = requests.get(
+        f"https://www.safegold.com/user-trends/gold-rates?start_date={today}&end_date={tomorrow}&frequency=d"
+    )
+
+    res_json: dict = res.json()
+
+    data: list[dict] = res_json.get('data')
+
+    # with open(filename, 'w') as file:
+    #     json.dump(data[-1], file)
+
+    return data[-1]
+
+
 
 # @app.route('/register', methods=['POST'])
 # def register():
